3d9c14f10bb3c4162c408b0ebd3af02544fd36c4
Cesar Villalba <cevillalba@frba.utn.edu.ar>
2025-04-17 16:46:17 +0000

    server sigue esperando

--- server/src/server.c
+++ server/src/server.c
@@ -1,35 +1,49 @@
 #include "server.h"
 
 int main(void) {
-	logger = log_create("/home/utnso/tp0/logs/tp0.log", "Servidor", 1, LOG_LEVEL_DEBUG);
+	logger = log_create("server.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 
 	int server_fd = iniciar_servidor();
 	log_info(logger, "Servidor listo para recibir al cliente");
 
 	int cliente_fd;
-	while (!cliente_fd)
-	{
-		cliente_fd = esperar_cliente(server_fd);
-	}
-
-	t_list* lista;
 	while (1) {
-		int cod_op = recibir_operacion(cliente_fd);
-		switch (cod_op) {
-		case MENSAJE:
-			recibir_mensaje(cliente_fd);
-			break;
-		case PAQUETE:
-			lista = recibir_paquete(cliente_fd);
-			log_info(logger, "Me llegaron los siguientes valores:");
-			list_iterate(lista, (void*) iterator);
-			break;
-		case -1:
-			log_error(logger, "el cliente se desconecto. Terminando servidor");
-			return EXIT_FAILURE;
-		default:
-			log_warning(logger,"Operacion desconocida. No quieras meter la pata");
-			break;
+		while (!cliente_fd)
+		{
+			cliente_fd = esperar_cliente(server_fd);
+		}
+
+		t_list* lista;
+		while (1) {
+			int cod_op = recibir_operacion(cliente_fd);
+			if (cod_op == -1)
+			{
+				log_error(logger, "el cliente se desconecto. esperando otro cliente");
+				cliente_fd = NULL;
+				break; // exit while 
+			}
+
+			if (cod_op == CLOSE_CONNECTION)
+			{
+				log_info(logger, "El cliente cerro la coneccion, esperando");
+				break; // exit while 
+			}
+			switch (cod_op) {
+			case MENSAJE:
+				recibir_mensaje(cliente_fd);
+				break;
+			case PAQUETE:
+				lista = recibir_paquete(cliente_fd);
+				log_info(logger, "Me llegaron los siguientes valores:");
+				list_iterate(lista, (void*) iterator);
+				break;
+			case -1:
+				log_error(logger, "el cliente se desconecto. Terminando servidor");
+				break; //return EXIT_FAILURE;
+			default:
+				log_warning(logger,"Operacion desconocida. No quieras meter la pata");
+				break;
+			}
 		}
 	}
 	return EXIT_SUCCESS;
